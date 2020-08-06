5400a998a30692bd3d3db0d8fe2d1fb7fda4bc0f
Alice Freda <alicefreda@gmail.com>
2020-08-06 13:44:49 -0700

    add some helpers for mock user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+    
+    helpers do
+        # mocked version of being logged in
+        def current_user
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
+
 end
