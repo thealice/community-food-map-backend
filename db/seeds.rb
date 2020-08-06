5aa0fb0daccae95374e494b3516c78034fd26135
Alice Freda <alicefreda@gmail.com>
2020-08-05 21:44:03 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+sf = Region.create(name: 'San Francisco', lat: 37.7576793, lng:-122.5076404, zoom:12)
+southbay = Region.create(name: 'South Bay', lat:37.2965315, lng:-122.0976061, zoom:10)
+eastbay = Region.create(name: 'East Bay', lat:37.7586346, lng:-122.5154696, zoom:10)
+northbay = Region.create(name: 'North Bay', lat:38.0675472, lng:-123.0211811, zoom:10)
+user = User.first
+opl = eastbay.locations.create(name: "Oakland Public Library - Main", notes: nil, lat: 0.37800978e2, lng: -0.122265797e3, zoom: 17, street: nil, city: nil, state: nil, user: user)
+lunch = opl.food_sources.create(name: 'Free Lunch', notes: 'Monday thru Friday 12pm-3pm', available: true, user: user)
+bateman  = eastbay.locations.create(lat:37.854866, lng:-122.255789, zoom:17 , street: '3069-3001 Bateman St' city: 'Berkeley' state: 'CA' user: user)
+pomtree = bateman.food_sources.create(name: 'Pomegranate Tree', notes: 'fruits in the fall', available: false, user: user)
+appletree = bateman.food_sources.create(name: 'Apple Tree', notes: 'fruits in the late summer/early fall', available: true, user: user)
