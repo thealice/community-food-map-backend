23a1abc5cdb9107c4d30fefe932184c9e63627ae
Alice Freda <alicefreda@gmail.com>
2020-08-05 21:44:56 -0700

    food sources will be delted when a location is deleted

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
     belongs_to :region
     belongs_to :user
+    has_many :food_sources, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     belongs_to :region
     has_many :food_sources
+    
     has_secure_password
 end
