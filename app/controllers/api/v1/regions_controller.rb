ba67092343957c4b7a9c684e317ce829d6603363
Alice Freda <alicefreda@gmail.com>
2020-08-06 13:39:24 -0700

    serialized json for region show and index

--- app/controllers/api/v1/regions_controller.rb
+++ app/controllers/api/v1/regions_controller.rb
@@ -1,11 +1,24 @@
 class Api::V1::RegionsController < ApplicationController
+    before_action :set_region, only: [:show]
+
     def index
         @regions = Region.all
-    
-        render json: @regions
-      end
+        regions_json = RegionSerializer.new(@regions).serialized_json
+        render json: regions_json
+    end
 
-      def show
-        render json: @region
-      end
+    def show
+        region_json = RegionSerializer.new(@region).serialized_json
+        render json: region_json  
+    end
+
+    private
+    
+    def set_region
+      @region = Region.find(params[:id])
+    end
+
+    def region_params
+      params.require(:region).permit(:name, :lat, :lng, :zoom)
+    end
 end
