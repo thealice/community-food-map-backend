6d50b019106ff478b30b7ded1ea268c3d20f8037
Alice Freda <alicefreda@gmail.com>
2020-08-06 13:44:49 -0700

    mocked version of being logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,12 @@
 class ApplicationController < ActionController::API
     
-    helpers do
-        # mocked version of being logged in
-        def current_user
-            User.first
-        end
+    # mocked version of being logged in
+    def current_user
+        User.first
+    end
 
-        def logged_in?
-            !!current_user
-        end
+    def logged_in?
+        !!current_user
     end
 
 end
