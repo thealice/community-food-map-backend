66b6ea4ff77a355380553c59c8a186f4fa28daca
Alice Freda <alicefreda@gmail.com>
2020-08-05 23:23:11 -0700

    add index and show actions to region

--- app/controllers/api/v1/regions_controller.rb
+++ app/controllers/api/v1/regions_controller.rb
@@ -1,2 +1,11 @@
 class RegionsController < ApplicationController
+    def index
+        @regions = Region.all
+    
+        render json: @regions
+      end
+
+      def show
+        render json: @region
+      end
 end
