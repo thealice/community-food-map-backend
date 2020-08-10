3469d96974ecb376984daf666fc3ad891def4901
Alice Freda <alicefreda@gmail.com>
2020-08-10 11:06:51 -0700

    add user serializer and edit user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create, :update, :delete]
+      resources :users, except: [:index]
       resources :regions, only: [:index, :show]
       resources :locations
       resources :food_sources
