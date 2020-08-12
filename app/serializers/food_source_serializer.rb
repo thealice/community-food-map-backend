4e1e3e5a4ca1660985816ecb4b55fa3292bf37b4
Alice Freda <alicefreda@gmail.com>
2020-08-12 13:07:47 -0700

    testing out custom attribute in serializer

--- app/serializers/food_source_serializer.rb
+++ app/serializers/food_source_serializer.rb
@@ -1,4 +1,12 @@
 class FoodSourceSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :notes, :available, :location_id, :user_id
+  attributes :name, :notes, :available, :location_id
+  
+  attribute :user do | food |
+    {
+      id: food.user.id,
+      name: food.user.name
+    }
+    
+  end
 end
