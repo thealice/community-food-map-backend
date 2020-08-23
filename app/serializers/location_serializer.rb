f0d8fbc9a7e2b89bab912928608ff59fc9339d5a
Alice Freda <alicefreda@gmail.com>
2020-08-23 14:17:47 -0700

    change Location's serialized id attribute to a string rather than integer

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,5 +1,8 @@
 class LocationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :notes, :lat, :lng, :zoom, :street, :city, :state, :zipcode, :food_sources, :user_id
+  attributes :name, :notes, :lat, :lng, :zoom, :street, :city, :state, :zipcode, :food_sources, :user_id
 
+  attributes :id do |location| 
+    location.id.to_s
+  end
 end
